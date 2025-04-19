b419ea149a9ad9396f9edcc17bf1be92442152d4
p.vanderwal <p.vanderwal@eurotransplant.org>
2025-04-19 15:12:10 +0200

    feat: gun now has a rate of fire of 1 bullet per second

--- bullet.gd
+++ bullet.gd
@@ -2,7 +2,16 @@ extends Node3D
 
 @export var speed: int = 20;
 
+@onready var rate_of_fire_timer = $Timer;
+var can_shoot = true;
+
+const KILL_TIME = 2;
+var timer = 0;
+
 
 func _physics_process(delta: float) -> void:
 	var forward_direction = global_transform.basis.z.normalized();
 	global_translate(forward_direction * speed * delta)
+	timer += delta;
+	if timer >= KILL_TIME:
+		queue_free();	can_shoot = false;
--- gun.gd
+++ gun.gd
@@ -4,7 +4,8 @@ extends Node3D
 
 @export var muzzle_speed = 30;
 @export var millis_between_shots = 100;
-
+@export var rate_of_fire_timer: Timer;
+var can_shoot = true;
 
 func _ready() -> void:
 	pass
@@ -13,7 +14,15 @@ func _process(delta: float) -> void:
 	shoot();
 	
 func shoot():
-	var new_bullet = Bullet.instantiate();
-	new_bullet.global_transform = $Muzzle.global_transform;
-	var sceneRoot = get_tree().root.get_child(0);
-	sceneRoot.add_child(new_bullet);
+	if can_shoot:
+		var new_bullet = Bullet.instantiate();
+		new_bullet.global_transform = $Muzzle.global_transform;
+		new_bullet.speed =muzzle_speed;
+		var sceneRoot = get_tree().root.get_child(0);
+		sceneRoot.add_child(new_bullet);
+		can_shoot = false;
+		rate_of_fire_timer.start();
+
+
+func _on_timer_timeout() -> void:
+	can_shoot= true
